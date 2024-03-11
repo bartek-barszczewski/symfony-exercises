99da6a76eefa5a1aa58e44872723b43b1689f167
Bart Barszczewski <154055563+bartek-barszczewski@users.noreply.github.com>
2024-03-11 20:43:28 +0100

    Exercise 1: Create app symfony, create two endpoint controllers

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/forecast/{threshold}', methods: ['POST'], requirements: ['threshold' => '\d+'])]
+    public function weather(int $threshold = 50): Response
+    {
+
+        $forecast = $threshold < 50 ? "It's going to be cold" : "It's going to be warm";
+
+        return $this->render('weather/index.html.twig', [
+            'forecast' => $forecast,
+        ]);
+    }
+
+    #[Route('/forecast/{guess}', methods: ['POST'])]
+    public function weather_guess(string $guess): Response
+    {
+
+        $forecast = "It's going to be " . $guess;
+
+        return $this->render('weather/index.html.twig', [
+            'forecast' => $forecast,
+        ]);
+    }
+}
